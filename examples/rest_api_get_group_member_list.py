2991b0fe08caa9490204a144bb71145c461cc5db
Josh Klahn <jdkforchrist@gmail.com>
2025-10-27 10:35:55 -0400

    Cleaned up group members list output

--- examples/rest_api_get_group_member_list.py
+++ examples/rest_api_get_group_member_list.py
@@ -1,4 +1,5 @@
 from guardiumATK import rest_api_functions
+import pandas
 
 """
 
@@ -12,7 +13,7 @@ Group API commands: https://www.ibm.com/docs/en/gdp/12.x?topic=commands-group-ap
 
 guardium_api = rest_api_functions.GuardiumRESTAPI(config_yaml_path="./test_config.yaml")
 
-# Example: List group members based on group name
+# Example: List group members based on group name ('description')
 result = guardium_api.get_group_members_by_desc(params={
     'desc': 'Sensitive Objects',  # [str] [required]; -- name of the group to get the list of members
     'api_target_host': ''  # [str]; -- Specifies the target hosts where the API executes. Examples:
@@ -20,4 +21,18 @@ result = guardium_api.get_group_members_by_desc(params={
     # 'all': execute on all managed units and the central manager
 })
 
-print(result)
+'''Example result:
+{'group_id': 5, 'group_description': 'Sensitive Objects', 'group_members': [{'member': '%/%.CUSTOMER'},... '''
+
+# extracting just the 'group_members' piece of the json result
+group_members_list_raw = result['group_members']
+
+'''Example group_members_list_raw
+[{'member': '%/%.CUSTOMER'}, {'member': '%/%.CUSTOMER_ADDRESS'}, {'member': '%/%.CUSTOMER_DEMOGRAPHICS'}...'''
+
+# raw list is a list of dictionaries - needs cleaned up to just be a list of strings
+group_members_list_cleaned = []
+for member in group_members_list_raw:
+    group_members_list_cleaned.append(member['member'])
+
+print(group_members_list_cleaned)  # cleaned up result - a list of strings for the group
